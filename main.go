eac58dbf4a4c2e5aeb3814b4e6ebe79c84bc68d4
dan4a <danshtal@gmail.com>
2024-12-22 23:07:19 +0300

    added HighCardTest

--- main.go
+++ main.go
@@ -3,6 +3,20 @@ package main
 // 2-10, J, Q, K, A
 // H-hearts, D-diamonds, S-spares, C-clubs
 
+// Combos:
+const (
+	HighCard      = "high card"
+	Pair          = "pair"
+	TwoPairs      = "two pairs"
+	ThreeOfAKind  = "three of a kind"
+	Straight      = "straight"
+	Flush         = "flush"
+	FullHouse     = "full house"
+	FourOfAKind   = "four of a kind"
+	StraightFlush = "straight flush"
+	RoyalFlush    = "royal flush"
+)
+
 type PokerHand struct {
 	cards []string
 }
@@ -11,4 +25,8 @@ func NewPokerHand(cards []string) *PokerHand {
 	return &PokerHand{
 		cards: cards,
 	}
+}
+
+func (p *PokerHand) GetCombo() string {
+	return HighCard
 }
--- main_test.go
+++ main_test.go
@@ -5,3 +5,8 @@ import "testing"
 func TestInitialPokerHand(t *testing.T) {
 	_ = NewPokerHand([]string{"2C", "9S", "8H", "4D", "5H"})
 }
+
+func TestGetCombo(t *testing.T) {
+	ph := NewPokerHand([]string{"2C", "9S", "8H", "4D", "5H"})
+	ph.GetCombo()
+}
