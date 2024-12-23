97fc37ce6176e85e7d0b4f8f12e493b2df7e3b65
dan4a <danshtal@gmail.com>
2024-12-23 10:36:37 +0300

    refactored tests

--- main_test.go
+++ main_test.go
@@ -2,44 +2,60 @@ package main
 
 import "testing"
 
-func TestHighCard(t *testing.T) {
-	ph := NewPokerHand([]string{"2C", "9S", "8H", "5H", "6H"})
-	assertString(t, HighCard, ph.GetCombo())
-}
+func TestPokerHands(t *testing.T) {
+	tests := []struct {
+		name  string
+		hand  []string
+		combo string
+	}{
+		{
+			"CanHighCard",
+			[]string{"2C", "9S", "8H", "5H", "6H"},
+			HighCard,
+		},
+		{
+			"CanPair",
+			[]string{"2C", "8H", "5H", "6H", "5D"},
+			Pair,
+		},
+		{
+			"CanTwoPairs",
+			[]string{"8C", "8H", "5H", "6H", "5D"},
+			TwoPairs,
+		},
+		{
+			"CanThreeOfAKind",
+			[]string{"5H", "5C", "5D", "8C", "9S"},
+			ThreeOfAKind,
+		},
+		{
+			"CanFlush",
+			[]string{"8C", "9C", "5C", "6C", "KC"},
+			Flush,
+		},
+		{
+			"CanFourOfAKind",
+			[]string{"5H", "5C", "5S", "5D", "8C"},
+			FourOfAKind,
+		},
+		{
+			"CanStraightFromAceToFive",
+			[]string{"AH", "2C", "3S", "4D", "5C"},
+			Straight,
+		},
+		{
+			"CanStraight",
+			[]string{"5H", "6C", "7S", "8D", "9C"},
+			Straight,
+		},
+	}
 
-func TestPair(t *testing.T) {
-	ph := NewPokerHand([]string{"2C", "8H", "5H", "6H", "5D"})
-	assertString(t, Pair, ph.GetCombo())
-}
-
-func TestTwoPairs(t *testing.T) {
-	ph := NewPokerHand([]string{"8C", "8H", "5H", "6H", "5D"})
-	assertString(t, TwoPairs, ph.GetCombo())
-}
-
-func TestThreeOfAKind(t *testing.T) {
-	ph := NewPokerHand([]string{"5H", "5C", "5D", "8C", "9S"})
-	assertString(t, ThreeOfAKind, ph.GetCombo())
-}
-
-func TestFlush(t *testing.T) {
-	ph := NewPokerHand([]string{"8C", "9C", "5C", "6C", "KC"})
-	assertString(t, Flush, ph.GetCombo())
-}
-
-func TestFourOfAKind(t *testing.T) {
-	ph := NewPokerHand([]string{"5H", "5C", "5S", "5D", "8C"})
-	assertString(t, FourOfAKind, ph.GetCombo())
-}
-
-func TestStraight(t *testing.T) {
-	ph := NewPokerHand([]string{"5H", "6C", "7S", "8D", "9C"})
-	assertString(t, Straight, ph.GetCombo())
-}
-
-func TestStraightFromAceToFive(t *testing.T) {
-	ph := NewPokerHand([]string{"AH", "2C", "3S", "4D", "5C"})
-	assertString(t, Straight, ph.GetCombo())
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ph := NewPokerHand(test.hand)
+			assertString(t, test.combo, ph.GetCombo())
+		})
+	}
 }
 
 func assertString(t *testing.T, expected, actual string) {
