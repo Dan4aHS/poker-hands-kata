3f5f39d5bd3b3d29a779208b51e703889ca4f9d7
dan4a <danshtal@gmail.com>
2025-01-04 00:06:05 +0300

    added card iterator

---
+++ poker-hand/card_iterator.go
@@ -0,0 +1,15 @@
+package poker_hand
+
+import "iter"
+
+func CardIterator() iter.Seq[Card] {
+	return func(yield func(Card) bool) {
+		for _, suit := range "HDSC" {
+			for _, value := range "23456789TJQKA" {
+				if !yield(NewCard(string(value) + string(suit))) {
+					return
+				}
+			}
+		}
+	}
+}
